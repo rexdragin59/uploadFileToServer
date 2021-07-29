7668b931bd19bc78a730b73f86dae3af649ffdb3
rexdragin59 <timesphere2@gmail.com>
2021-07-29 00:19:35 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<html>
+<body>
+	<form action="process.php" enctype="multipart/form-data" method="post">
+		<input type="text" id="fname1" name="fname2" value="John"><br>
+		<input type="text" id="lname1" name="lname2" value="Doe"><br><br>
+		<input type="file" name="myFile2a">
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
